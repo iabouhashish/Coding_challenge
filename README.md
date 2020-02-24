a6a95ec3e9fa11014ced2f2d2f7f9215d56b79db
Ibrahim Abouhashish <ibrahimabouhashish@gmail.com>
2020-02-23 19:23:50 -0500

    Important changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Coding Challenge for Luminari
+# Coding Challenge
 
 ####Dependencies
 To install dependencies, run 
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Coding Challenge",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/iabouhashish/Coding_challenge",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
